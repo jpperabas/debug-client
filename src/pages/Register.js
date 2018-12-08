95f8eeb5d267fd1321347276300175f67409cee3
Juan Pablo Pérez Abascal <jpperabas@gmail.com>
2018-12-08 13:46:20 -0600

    Varios

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 
 import { RegisterForm } from '../modules/General'
+import { Container } from '../elements/Containers'
 
 class Register extends Component {
   render() {
     return (
-      <div>
+      <Container center>
         <RegisterForm />
-      </div>
+      </Container>
     )
   }
 }
